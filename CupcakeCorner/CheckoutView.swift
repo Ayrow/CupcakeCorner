3ce6ccbca8b3f45c93976668686f0875431a88c9
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-06 11:52:33 +0100

    Setup the checkout page

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     var body: some View {
-        Text("Hello World")
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(height: 233)
+                    
+                    Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                        .font(.title)
+                    
+                    Button("Place order") {
+                        //
+                    }
+                    .padding()
+                }
+                
+            }
+            .navigationTitle("Checkout")
+            .navigationBarTitleDisplayMode(.inline)
+        }
 
         }
 }
