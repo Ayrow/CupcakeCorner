6cc7a1e9911081822b7c26c06eb8286ef7912f89
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-06 10:25:50 +0100

    Setup an order class

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,15 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var order = Order()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("")
+        
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
